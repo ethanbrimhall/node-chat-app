06287b7666047a9c0730be56f54ceea1618e7cf9
Ethan <realethanbrimhall@gmail.com>
2018-05-29 12:44:45 -0400

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -13,14 +13,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User Connected');
     
-    socket.emit('newMessage', {
-        from: 'ethan',
-        text: 'whatsup',
-        createdAt: 123123
-    });
-    
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     });
     
     socket.on('disconnect', () => {
