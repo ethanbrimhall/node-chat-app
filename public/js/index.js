b7cf68b35f3e3f26584286c241e3075451336596
Ethan <ethannbrimhall@gmail.com>
2018-05-29 13:08:10 -0400

    Added comments to the code

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,15 @@
 var socket = io();
-            
+
+//Called when user connects
 socket.on('connect', function() {
     console.log('Connected to server'); 
     
 });
-            
+//Called when user disconnects            
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
-
+//Called when there is a new message from server
 socket.on('newMessage', function(message){
     console.log('newMessage', message);
 });
