5407d5b714efd3a2386109590ed824172d6eb75b
Ethan <ethannbrimhall@gmail.com>
2018-05-29 16:44:32 -0400

    Added css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -34,12 +34,14 @@ socket.on('newLocationMessage', function(message){
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
     
+    var messageTextBox = jQuery('[name=message]');
+    
     //sends message to server
     socket.emit('createMessage', {
         from: 'user',
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function(data){
-        
+        messageTextBox.val('');
     });
 });
 
@@ -50,12 +52,16 @@ locationButton.on('click', function(){
         return alert('Geolocation not supported by your browser!');
     }
     
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
+    
     navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function(){
+        locationButton.removeAttr('disabled');
         alert('Unable to fetch location!')
     });
 });
