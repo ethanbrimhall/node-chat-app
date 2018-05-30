ead96e4edf64df1b6679373ff9993d038958692e
Ethan <ethannbrimhall@gmail.com>
2018-05-29 20:34:34 -0400

    Added automatic scrolling to message box

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom(){
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 //Called when user connects
 socket.on('connect', function() {
     console.log('Connected to server'); 
@@ -20,6 +36,7 @@ socket.on('newMessage', function(message){
     });
     
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 //Gets location from server and prints out url to user
@@ -33,6 +50,7 @@ socket.on('newLocationMessage', function(message){
     });
     
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 //When the send button is clicked
